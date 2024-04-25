5f40995e6ddd21116480e58893e25fad44b8a6c5
jay-java <jayprajapati.tops@gmail.com>
2024-04-25 10:08:50 +0530

    newlaravelproject

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/',function (){
+        return view('home');
+    });
 });
